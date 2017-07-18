cfeb9e7f8f150900a3ea38e9052dc27f897b39b3
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-18 16:31:37 +0000

    integrated helper methods from actionView

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20170712150750) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status", default: 0
-    t.integer "topic_id"
+    t.bigint "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
     t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
