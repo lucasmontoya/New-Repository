391ba4a8df4bfb730de4eb101a48ea5cb633016a
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-27 15:50:26 +0000

    updated file upload forms and refactored placeholder functionality into view

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 20170725174431) do
 
   create_table "skills", force: :cascade do |t|
     t.string "title"
-    t.integer "percent_utitlized"
+    t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "badge"
