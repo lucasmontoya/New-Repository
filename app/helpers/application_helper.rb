ca969c0b48dc1eb8b1ab62bbb7dd7d38a34ff32d
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-31 03:03:20 +0000

    refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,4 +59,16 @@ module ApplicationHelper
         "active" if current_page? path
     end
     
+    def alerts
+        alert = (flash[:alert] || flash[:notice] || flash[:error])
+        
+        if alert
+            alert_generator alert
+        end
+    end
+    
+    def alert_generator msg
+        js add_gritter(msg, title: "Lucas Montoya Portfolio", sticky: false)
+    end
+    
 end
